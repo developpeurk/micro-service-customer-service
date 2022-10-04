bc8c527e674d20e776f685e668a88f65d0666d98
softengyas <y.lambarki@etu.enset-media.ac.ma>
2022-10-04 16:14:31 +0100

    initial project

---
+++ src/main/java/com/lambarki/yassine/microservicecustomerservice/MicroServiceCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lambarki.yassine.microservicecustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceCustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceCustomerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lambarki/yassine/microservicecustomerservice/MicroServiceCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambarki.yassine.microservicecustomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceCustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
