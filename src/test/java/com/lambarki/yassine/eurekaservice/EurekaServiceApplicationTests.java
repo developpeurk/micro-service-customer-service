5b982818b025a2d3ec47a1b6f8a6705784e57c2c
softengyas <y.lambarki@etu.enset-media.ac.ma>
2022-10-04 23:16:32 +0100

    Eureka configuration

---
+++ src/test/java/com/lambarki/yassine/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambarki.yassine.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
